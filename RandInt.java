405fcfce78711c5ee514c27b875b10ed983779b3
Her0 <79155428+Her0-GitHub@users.noreply.github.com>
2023-05-10 11:28:17 +0200

    Add JavaDoc

--- RandInt.java
+++ RandInt.java
@@ -1,13 +1,26 @@
 import java.util.Random;
 
 public class RandInt {
-    private static Random rand = new Random();
-    public static int tra(int min, int max){
+    private static final Random rand = new Random();
+    /**
+     * @param min Minimum Value (Included)
+     * @param max Maximum Value (Included)
+     * @return Random number between min and max
+     */
+    public static int tra(int min, int max) {
         return (int) Math.round(rand.nextDouble() * (max - min) + min);
     }
+    /**
+     * @param max Maximum Value (Included)
+     * @return Random number between 0 and max
+     */
     public static int fromZero(int max) {
         return (int) Math.round(rand.nextDouble() * max);
     }
+    /**
+     * @param max Maximum Value (Included)
+     * @return Random number between 1 and max
+     */
     public static int fromOne(int max) {
         return (int) Math.round(rand.nextDouble() * (max - 1) + 1);
     }
